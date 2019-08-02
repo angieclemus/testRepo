bff0a98b4f69a1a2fd5bec71774f78b524ce1a7d
troperc <51921895+troperc@users.noreply.github.com>
2019-08-02 15:26:20 -0500

    Se agrego la fiunconalidad de agregar intentos

--- src/main/java/edu/upb/promg2/testrepo/Juego.java
+++ src/main/java/edu/upb/promg2/testrepo/Juego.java
@@ -5,22 +5,29 @@ import java.util.Scanner;
 
 public class Juego 
 {
+
     public static void main(String[] args) 
     {
-        System.out.println("Dame un número entre 1-20: ");
+        int contador=0;
+        int num;
+        int numAleatorio;
         Scanner teclado = new Scanner(System.in);
-        int num = Integer.parseInt(teclado.nextLine());
-        
         Random r = new Random();
-        int numAleatorio = r.nextInt(20)+1;
-        
-        if(num == numAleatorio)
+
+        do 
         {
-            System.out.println("Ganaste con el número: "+ num); 
-        }
-        else
-        {
-            System.out.println("Pailas al número correcto era " + numAleatorio + "y no " + num);
-        }
+            System.out.println("Dame un número entre 1-20: ");
+
+            num = Integer.parseInt(teclado.nextLine());
+            numAleatorio = r.nextInt(20) + 1;
+            if (num == numAleatorio) {
+                System.out.println("Ganaste con el número: " + num);
+            } else {
+                System.out.println("Pailas al número correcto era: " + numAleatorio + "y no: " + num);
+            }
+            contador++;
+        } 
+        while (num != numAleatorio);
+        System.out.println("Ganaste despues de " + contador );
     }
 }
